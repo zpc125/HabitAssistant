3da7fb1e91b4716e9570bdffb240f74eef8a7e15
“lzy1010” <1487207414@qq.com>
2023-07-09 14:24:38 +0800

    获取gps天气

--- app/src/main/java/com/example/habitassistant/MainActivity.java
+++ app/src/main/java/com/example/habitassistant/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.habitassistant;
 
+import static android.content.ContentValues.TAG;
 import static android.hardware.Sensor.TYPE_GYROSCOPE;
 
+import android.Manifest;
 import android.app.AppOpsManager;
 import android.app.usage.UsageStats;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -22,6 +27,7 @@ import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.WindowCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -29,6 +35,13 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.habitassistant.databinding.ActivityMainBinding;
+import com.google.gson.Gson;
+import com.qweather.sdk.bean.base.Code;
+import com.qweather.sdk.bean.base.Lang;
+import com.qweather.sdk.bean.base.Unit;
+import com.qweather.sdk.bean.weather.WeatherNowBean;
+import com.qweather.sdk.view.HeConfig;
+import com.qweather.sdk.view.QWeather;
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -60,6 +73,17 @@ public class MainActivity extends AppCompatActivity implements SensorHandler.Sen
     private double accelerZ;
     private boolean screenStatus;
 
+    private String weather;
+    private String windScale;
+    private String windSpeed;
+
+    private String latitude;
+    private String longitude;
+    private LocationManager locationManager;
+
+    private String place;
+    private String action;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,8 +99,8 @@ public class MainActivity extends AppCompatActivity implements SensorHandler.Sen
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String place = (String) placeSpinner.getSelectedItem();
-                String action = (String) actionSpinner.getSelectedItem();
+                place = (String) placeSpinner.getSelectedItem();
+                action = (String) actionSpinner.getSelectedItem();
                 Toast.makeText(MainActivity.this, place, Toast.LENGTH_SHORT).show();
                 Toast.makeText(MainActivity.this, action, Toast.LENGTH_SHORT).show();
 //                Intent startIntent = new Intent(MainActivity.this, MyService.class);
@@ -88,6 +112,15 @@ public class MainActivity extends AppCompatActivity implements SensorHandler.Sen
 
         //屏幕是否亮
         screenStatusChecker = new ScreenStatusChecker(this);
+
+        //位置传感器
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//        location();
+
+        //连接天气API
+        HeConfig.init("HE2307051114281793", "8055206813554fa99ce7e0a115b15683");
+        //切换至免费订阅
+        HeConfig.switchToDevService();
 
         // 开始循环
         putTable();
@@ -158,6 +191,8 @@ public class MainActivity extends AppCompatActivity implements SensorHandler.Sen
             @Override
             public void run() {
                 screenStatus = screenStatusChecker.isScreenOn();
+                getGPS();
+                getTheater();
                 // 检查并打印屏幕状态
 //                if (screenStatusChecker.isScreenOn()) {
 //                    Log.i("ScreenStatus", String.valueOf(screenStatus));
@@ -168,9 +203,12 @@ public class MainActivity extends AppCompatActivity implements SensorHandler.Sen
                 //陀螺仪x,y,z：gyroscopeX,gyroscopeX,gyroscopeZ
                 //加速度器x,y,z：accelerX,accelerY,accelerZ
                 //屏幕状态：screenStatus（true/false）
-
+                //天气:weather,windScale,windSpeed
+                //GPS:latitude,longitude
+                //标签：place,action
                 //以上是数据，直接写入table
 
+                System.out.println(weather+"  "+windScale+"  "+windSpeed+"  "+latitude+"  "+longitude);
                 // 重新安排代码在10秒后再次运行
                 handler.postDelayed(this, 10000);
             }
@@ -185,5 +223,87 @@ public class MainActivity extends AppCompatActivity implements SensorHandler.Sen
         super.onDestroy();
         // 当活动销毁时，取消定时操作，以避免内存泄漏
         handler.removeCallbacksAndMessages(null);
+    }
+
+
+    //天气
+    public void getTheater() {
+        double number = Double.parseDouble(latitude);
+        String lat = (int) number + "." + String.format("%02d", (int) ((number - (int) number) * 100));
+        double number1 = Double.parseDouble(longitude);
+        String lon = (int) number + "." + String.format("%02d", (int) ((number - (int) number) * 100));
+
+        String xy=lat+","+lon;
+        Log.i("MainActivity",xy);
+        QWeather.getWeatherNow(MainActivity.this, xy, Lang.ZH_HANS, Unit.METRIC,
+                new QWeather.OnResultWeatherNowListener() {
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.i(TAG, "getWeather onError: " + e);
+                    }
+
+                    @Override
+                    public void onSuccess(WeatherNowBean weatherNowBean) {
+                        //先判断返回的status是否正确，当status正确时获取数据，若status不正确，可查看status对应的Code值找到原因
+                        if (Code.OK == weatherNowBean.getCode()) {
+                            WeatherNowBean.NowBaseBean now = weatherNowBean.getNow();
+                            weather = String.valueOf(weatherNowBean.getNow().getText());
+                            windScale = String.valueOf(weatherNowBean.getNow().getWindScale());
+                            windSpeed = String.valueOf(weatherNowBean.getNow().getWindSpeed());
+                            //由于是在非主线程中更新文本信息，所以需要调用主线程方法
+//                            runOnUiThread(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    tianqi.setText("天气" + now.getText());
+//
+//                                }
+//                            });
+
+                        } else {
+                            //在此查看返回数据失败的原因
+                            Code code = weatherNowBean.getCode();
+                            Log.i(TAG, "failed code: " + code);
+                        }
+                    }
+
+                });
+    }
+
+    //GPS
+    public void getGPS() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            Log.i("Permission-手动", "GPS权限未开启");
+            //获取权限
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION},
+                    1);
+            return;
+        }
+        // location();
+
+        //3种获取GPS实例方法，从网络，到GPS，再到被动获取的顺序
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        if (location==null){
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (location==null){
+                location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+            }
+        }
+        //输出经纬度
+        if(location!=null){
+            //获取经纬度
+            latitude = String.valueOf(location.getLatitude());
+            longitude = String.valueOf(location.getLongitude());
+        }else {
+            Log.i("Permission", "传感器为空");
+            Log.i("Permission", String.valueOf(location));
+        }
+
     }
 }
